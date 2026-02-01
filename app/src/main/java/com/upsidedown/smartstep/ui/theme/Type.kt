1da4aff22801c7405927e3a0a6484b10736276e7
jatinsinghal <jatinsinghal404@gmail.com>
2026-02-01 19:02:34 +0530

    implement typography

--- app/src/main/java/com/upsidedown/smartstep/ui/theme/Type.kt
+++ app/src/main/java/com/upsidedown/smartstep/ui/theme/Type.kt
@@ -2,19 +2,64 @@ package com.upsidedown.smartstep.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.upsidedown.smartstep.R
 
 // Set of Material typography styles to start with
+
+val inter = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.inter,
+            weight = FontWeight.Normal,
+        ),
+        Font(
+            resId = R.font.inter_medium,
+            weight = FontWeight.Medium
+        ),
+        Font(
+            resId = R.font.inter_semibold,
+            weight = FontWeight.SemiBold
+        ),
+    )
+)
 val Typography = Typography(
+
+    titleLarge = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 64.sp,
+        lineHeight = 70.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp
+    ),
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = inter,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 24.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -32,3 +77,18 @@ val Typography = Typography(
     )
     */
 )
+
+val Typography.bodyLargeMedium: TextStyle
+    get() = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    )
+val Typography.bodyMediumMedium: TextStyle
+    get() = TextStyle(
+        fontFamily = inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    )
