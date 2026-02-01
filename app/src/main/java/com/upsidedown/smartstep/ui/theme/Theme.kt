bf3a40cc885626e4a688070bc5df2a9e8c3e69f7
jatinsinghal <jatinsinghal404@gmail.com>
2026-02-01 18:40:05 +0530

    implement theming

--- app/src/main/java/com/upsidedown/smartstep/ui/theme/Theme.kt
+++ app/src/main/java/com/upsidedown/smartstep/ui/theme/Theme.kt
@@ -11,17 +11,19 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
+    primary = ButtonPrimary,
+    onPrimary = TextWhite,
+    secondary = ButtonSecondary,
+    onSecondary = TextPrimary,
+    background = BackgroundMain,
+    surface = BackgroundSecondary,
+    surfaceVariant = BackgroundTertiary,
+    onSurface = TextPrimary,
+    onSurfaceVariant = TextPrimary,
+    inverseOnSurface = TextSecondary,
+    outline = StrokeMain,
+    surfaceContainer = BackgroundWhite
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -40,18 +42,9 @@ fun SmartStepTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = LightColorScheme,
         typography = Typography,
         content = content
     )
